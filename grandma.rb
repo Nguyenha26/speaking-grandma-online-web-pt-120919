27caa27ee7a4a4206e8c607e0aaca30b34253925
Nguyen Ha <nynha0526@gmail.com>
2019-12-15 23:52:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,6 @@
+def speak_to_grandma("I love you grandma!".upcase)
+
+
 if speak_to_grandma == "I love you grandma!".upcase
   puts "I love you too pumpkin!".upcase
 elsif speak_to_grandma == "You're awesome grandma.".upcase
@@ -6,4 +9,4 @@ else
   puts "No, not since 1938!".upcase
 end
 
-speak_to_grandma = "I love you grandma!".upcase
+
